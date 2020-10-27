00e78c64838a679d5ef12ed80228ae334a1e3dc8
dev3dmitriy.d <dev3dmitriy.d@alldigitalads.com>
2020-10-27 19:44:48 +0200

    Add interceptor returning Response with `data` wrapper

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
+import {WrapResponseInterceptor} from "./common/interceptors/wrap-response.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,11 @@ async function bootstrap() {
         }
       })
   );
+
+    app.useGlobalInterceptors(
+        new WrapResponseInterceptor(),
+    );
+
   await app.listen(3000);
 }
 bootstrap();
