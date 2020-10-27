5b9464048160984ef1b473d5406657fa5441cd7e
dev3dmitriy.d <dev3dmitriy.d@alldigitalads.com>
2020-10-27 19:52:27 +0200

    Add interceptor returning Response with `data` wrapper [only GET method]

--- src/common/interceptors/wrap-response.interceptor.ts
+++ src/common/interceptors/wrap-response.interceptor.ts
@@ -5,6 +5,9 @@ import {map} from "rxjs/operators";
 @Injectable()
 export class WrapResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle().pipe(map((data) => ({ data })));
+    if (context.switchToHttp().getRequest().method === 'GET') {
+      return next.handle().pipe(map((data) => ({ data })));
+    }
+    return next.handle();
   }
 }
