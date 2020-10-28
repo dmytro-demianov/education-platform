81c3794b462cea2d5eb6be51ff1c34a627a734d7
dev3dmitriy.d <dev3dmitriy.d@alldigitalads.com>
2020-10-28 22:36:36 +0200

    Install swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 import {WrapResponseInterceptor} from "./common/interceptors/wrap-response.interceptor";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -22,6 +23,15 @@ async function bootstrap() {
     app.useGlobalInterceptors(
         new WrapResponseInterceptor(),
     );
+
+    const options = new DocumentBuilder()
+        .setTitle('Education')
+        .setDescription('Final project: education platform')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
 
     await app.listen(3000);
 }
